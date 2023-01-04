d65e3d6d3b4fe7c3b8b223e49e3d331c80eb6df6
Mykola Saliuk <haker0502@ukr.net>
2023-01-04 18:28:40 +0200

    вносить правки

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -5,7 +5,9 @@ import css from './FriendListItem.module.css';
 export const FriendListItem = ({ avatar, name, isOnline }) => {
   return (
     <li className={css.item}>
-      <span className={isOnline ? css.isOnline : css.status}></span>
+      <span
+        className={isOnline ? `${css.status} ${css.isOnline}` : css.status}
+      ></span>
       {/* <span className={clsx(css.status, { [css.isOnline]: isOnline })}></span> */}
       <img className={css.avatar} src={avatar} alt={name} width="48" />
       <p className={css.name}>{name}</p>
